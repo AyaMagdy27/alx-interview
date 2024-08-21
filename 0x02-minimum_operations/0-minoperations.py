bb1e62663f039ce24fd985ab888fc958c746a77c
AyaMagdy27 <ayamado1996@gmail.com>
2024-08-21 09:20:00 +0000

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
+""" Minimum Operations """
 
 
 def minOperations(n: int) -> int:
     """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
+    if n <= 1:
+        return 0
+
     op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
+    body_len = 1  # Starting with one 'H'
+    
+    while body_len < n:
+        # If n is divisible by current length of body, do "Copy All" and "Paste"
+        if n % body_len == 0:
+            op += 2  # 1 for "Copy All" and 1 for "Paste"
+            body_len *= 2
         else:
+            # If not divisible, just do "Paste"
             op += 1
-            body += next
-    if len(body) != n:
+            body_len += body_len
+    
+    if body_len != n:
         return 0
     return op
