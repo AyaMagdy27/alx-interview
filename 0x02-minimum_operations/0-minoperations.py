d8d3a62ded77934e20866036f48e010161ccafc2
AyaMagdy27 <ayamado1996@gmail.com>
2024-08-21 09:26:09 +0000

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-""" Minimum Operations """
+
+""" Has a single character H with two operations: Copy All and Paste
+    Give number n, write a method that calculates operations to result
+    in exactly n H.
+    Prototype: def minOperations(n)
+    Return an integer
+    if n is impossible to achieve, return 0
+    """
 
 
-def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
+def minOperations(n):
+    """
+    Single character H
+    Fewest number of operations
+    """
+
     if n <= 1:
         return 0
+    numbr, index, operations = n, 2, 0
 
-    op = 0
-    body_len = 1  # Starting with one 'H'
-    
-    while body_len < n:
-        # If n is divisible by current length of body, do "Copy All" and "Paste"
-        if n % body_len == 0:
-            op += 2  # 1 for "Copy All" and 1 for "Paste"
-            body_len *= 2
+    while numbr > 1:
+        if numbr % index == 0:
+            numbr = numbr / index
+            operations = operations + index
         else:
-            # If not divisible, just do "Paste"
-            op += 1
-            body_len += body_len
-    
-    if body_len != n:
-        return 0
-    return op
+            index += 1
+    return operations
